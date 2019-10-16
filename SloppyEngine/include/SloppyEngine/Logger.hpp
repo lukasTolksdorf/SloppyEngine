5d5be315c4df740f1e7d8bbdf33aac86542cf218
Lukas Tolksdorf <Lukas.Tolksdorf@gmail.com>
2019-10-16 17:10:25 +0200

    Basic logging functionality for core and app

--- SloppyEngine/include/SloppyEngine/Logger.hpp
+++ SloppyEngine/include/SloppyEngine/Logger.hpp
@@ -5,13 +5,29 @@
 #ifndef SLOPPYENGINE_LOGGER_HPP_
 #define SLOPPYENGINE_LOGGER_HPP_
 
+#include "spdlog/spdlog.h"
+#include "spdlog/fmt/ostr.h"
+
 namespace Sloppy {
 namespace Core {
 
 class Logger {
 public:
   static void Init();
+
+  static std::shared_ptr<spdlog::logger> getCoreLogger() {
+    return coreLogger_;
+  }
+
+  static std::shared_ptr<spdlog::logger> getClientLogger() {
+    return clientLogger_;
+  }
+
+private:
+  static std::shared_ptr<spdlog::logger> coreLogger_;
+  static std::shared_ptr<spdlog::logger> clientLogger_;
 };
+
 }
 }
 
--- SloppyEngine/src/Logger.cc
+++ SloppyEngine/src/Logger.cc
@@ -4,10 +4,22 @@
 
 #include "SloppyEngine/Logger.hpp"
 
-#include <iostream>
+#include "spdlog/sinks/stdout_color_sinks.h"
 
 namespace Sloppy {
 namespace Core {
-void Logger::Init() { std::cout << "Logger init done!\n"; }
+
+std::shared_ptr<spdlog::logger> Logger::coreLogger_;
+std::shared_ptr<spdlog::logger> Logger::clientLogger_;
+
+void Logger::Init() {
+
+  spdlog::set_pattern("%^[%T] %n: %v%$");
+  coreLogger_ = spdlog::stdout_color_mt("SLOPPY");
+  coreLogger_->set_level(spdlog::level::trace);
+
+  clientLogger_ = spdlog::stdout_color_mt("APP");
+  clientLogger_->set_level(spdlog::level::trace);
+}
 } // namespace Core
 } // namespace Sloppy
